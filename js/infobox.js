aad505caaeb4813eaa6ca3f7dd80c6231c4b77ca
veryrarestat <stmantaz@gmail.com>
2024-05-10 23:02:40 +0100

    V1.2.2
    Fixed/Changed: Small style change is not major enough to make a new version.

--- js/infobox.js
+++ js/infobox.js
@@ -14,7 +14,6 @@ cover.classList.add('cover');
 document.body.appendChild(cover);
 
 infoBox.innerHTML = `
-    <p class="beta-text">Please note this is still majorly in Beta and may experience some issues</p>
     <div class="info-box-title">
         <button class="close-button">x</button>
         <h2>Welcome to Recipify!</h2>
@@ -27,6 +26,7 @@ infoBox.innerHTML = `
             <li><p>This will generate a recipe along the selection of your ingredients but will add extra ingredients</p></li>
         </ul>
     </div>
+    <p class="beta-text">Please note this is still majorly in Beta and may experience some issues</p>
     <p>If you have any recipes you would like me to add contact me via <a href="mailto:mantas@stkvs.com">My Email</a></p>
 `;
 
